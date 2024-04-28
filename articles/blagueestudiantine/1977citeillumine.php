f92324d05d7817bc27999f0af025df787b69869f
BlueTorche <dubrulle_kevin@hotmail.be>
2024-04-28 13:23:14 +0200

    Adds some warning because they were necessary oupsis

--- articles/blagueestudiantine/1977citeillumine.php
+++ articles/blagueestudiantine/1977citeillumine.php
@@ -4,7 +4,7 @@ $id = "cité1977";
 baseArticle($title,$id);
 ?>
 
-<img src="/image/blagueestudiantine/cite69.jpg" alt="69" width="300px" class="img-right">
+<img src="/image/blagueestudiantine/cite69.jpg" alt="69" width="270" class="img-right">
 <p>
     C’est durant une belle nuit de revue que la cité s’illumina de mille feux, laissant aparaitre cette majestueuse figure. Avec l’aide de nos Fredo et des ouvriers de l’époque, Talkie-Walkie à la main, les étudiants ce livrerent à ce petit jeu qui devrait se relancer un peu plus souvent… Ahhhhhh la belle époque ou tout le monde était la pour s’amuser et se faire plaisir, va-t-en demander cela à certains locataires actuels de la cite, super sympathique et ouverts d’esprit de laisser allumé ou éteint juste quelques minutes…
 </p>
