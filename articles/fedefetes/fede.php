33924193abf269aaef04a9b7d4aeddb8a70bc92a
BlueTorche <dubrulle_kevin@hotmail.be>
2024-08-22 15:08:03 +0200

    adds two article directories

---
+++ articles/fedefetes/fede.php
@@ -0,0 +1,9 @@
+<?php
+$title ="Fédération des Étudiants de la F.P.Ms";
+$id = "fede";
+baseArticle($title,$id);
+?>
+
+<p>
+    Todo
+</p>
---
+++ articles/regionales/carolo.php
@@ -0,0 +1,9 @@
+<?php
+$title ="Carolo";
+$id = "carolo";
+baseArticle($title,$id);
+?>
+
+<p>
+    Todo
+</p>
