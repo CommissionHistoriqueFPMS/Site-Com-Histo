7cd114f5fd0897305aaf4008ed8ad5992e63094f
BlueTorche <dubrulle_kevin@hotmail.be>
2024-09-06 15:55:53 +0200

    correction d'un faute d'orthographes

--- vieetudiante.php
+++ vieetudiante.php
@@ -1,9 +1,9 @@
 <?php
 include("./include-php/fonctions.php");
 generatePage("Vie Étudiante", ["Vie Étudiante"], ["vieetudiante"], ["
-                <p>
-                    Tout bon étudiant, comme son folklore, se caractérise par des habitudes de vie bien particulières,
-                    avec comme grands évènements...
-                </p>
-                "]);
+        <p>
+            Tout bon étudiant, comme son folklore, se caractérise par des habitudes de vie bien particulières,
+            avec comme grands évènements...
+        </p>
+        "]);
 ?>
