319b829e3bedaf6b38387e4edd112114fa8668b8
BlueTorche <dubrulle_kevin@hotmail.be>
2024-09-15 10:47:01 +0200

    Create articles diversion

--- articles/blagueestudiantine/1974diversions.php
+++ articles/blagueestudiantine/1974diversions.php
@@ -4,4 +4,37 @@ $id = "diversion74_79";
 baseArticle($title,$id);
 ?>
 
-<?php defaultArticle(); ?>
+<p>Durant les années 70, les étudiants de la F.P.Ms se firent remarquer grâce à de nombreuses blagues aujourd’hui célèbre.</p>
+<?php addSource("Nous nous excusons pour la qualité de certaines images :(") ?>
+<h4>Ravitaillement Pétrolier - 1974</h4>
+<p>Les citoyens étaient en effet invités à rendre un questionnaire sur la consommation de leur énergie afin de recevoir des tickets de ravitaillement sur le pétrole, mazout et autres produits dérivés ! Le questionnaire ne manque pas de piquant et on n’hésite pas à leur demander, entre autres, la température de leur bain, la charge moyenne dans leur voitures, etc…</p>
+<?php addImage("/image/blagueestudiantine/petrol1.webp", 400, "left") ?>
+<?php addImage("/image/blagueestudiantine/petrol2.webp", 400, "right") ?>
+<div class="clear"></div>
+
+<h4>Péage dans la Ville - 1976</h4>
+<p>Evidemment, il fallait y penser: trop de voitures et d’encombrement dans la ville, il fallait bien sûr instaurer un système de péage pour calmer les nombreux visiteurs de la ville de Mons! Il suffisait d’aller chercher sa carte de riverain pour éviter ces péages.</p>
+
+
+<?php addImage("/image/blagueestudiantine/peage1.webp", 250, "left") ?>
+<?php addImage("/image/blagueestudiantine/peage2.webp", 250, "left") ?>
+<?php addImage("/image/blagueestudiantine/peage3.webp", 250, "left") ?>
+<div class="clear"></div>
+
+<h4>G. Ripsou Vérifie vos Contributions Fiscales - 1977</h4>
+<p>L’ordinateur qui plante et Bardaf, c’est l’embardée… Il faut vite aller aux bureaux des contributions pour vérifier sa déclaration avec ce charmant G. Ripsou !</p>
+<?php addImage("/image/blagueestudiantine/Gripsou.webp", 400, "center") ?>
+<div class="clear"></div>
+
+<h4>Eau Empoisonée - 1978</h4>
+<p>La station de captage de Cuesmes a quelques défaillances et voici l’eau du robinet contaminée… Libre à chacun d’aller chercher des capsules de neutralisant à la santé publique !</p>
+
+<?php addImage("/image/blagueestudiantine/eau.webp", 400, "center") ?>
+<div class="clear"></div>
+
+
+<h4>Capteurs Solaires Gratuit - 1979</h4>
+<p>Hé oui, on parlait déjà d’énergies renouvelable à l’époque! Et les experts vous ont bien sûr choisi comme cobaye!</p>
+<?php addImage("/image/blagueestudiantine/solaire.webp", 400, "center") ?>
+<div class="clear"></div>
+
