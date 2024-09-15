552b0ef6b06e80a0386772d4ce179f97cfd6b7b3
BlueTorche <dubrulle_kevin@hotmail.be>
2024-09-15 14:17:30 +0200

    adds some commentary of stuffs in scientifique.php

--- articles/cerclefedere/scientifique.php
+++ articles/cerclefedere/scientifique.php
@@ -5,3 +5,62 @@ baseArticle($title,$id);
 ?>
 
 <?php defaultArticle(); ?>
+
+
+<!--
+Hisoire de Chars
+Bon, comme vous l'aurez compris, vous retrouverez ici tous les chars de bleusaille...
+Je m'efforcerai de les trier au fur et à mesure... Si certaines photos ou intitulés ne sont pas encore présents, ils ne tarderont pas à être mis à jour... Les photos n'ont plus qu'à être scannées...
+Voici déjà les derniers en date, en commençant par le plus récent...
+Les chars sont devenus, au fil des temps, un emblême du cortège. Il est impossible de penser que la Polytech viendra au cortège de bleusaille sans son char! Au fur et à mesure, les véhicules se sont de plus en plus recouverts, jusqu'à définitivement disparaître sous l'ornement qui les hornent. Quelques phots des différents chars sont reprises ci-dessous, parfois même lors de leur construction.
+Viendront ensuite les objets divers accompagnant le char: comme le char à p****, objet nécessaire et indispensable à tout cortège qui se respecte, toujours monté et peint à la va-vite avec "ce qui reste" 10min avant l'arrivée des togés!
+Il suffit de clicker sur les photos pour qu'elles s'affichent en grand!
+
+Le 169 (2007): ... Le 13² ... A venir...
+Le 168 (2006): Le Dumper Caterpillar (jaune)
+Le 167 (2005): La locomotive à vapeur
+Le 166 (2004): La catapulte
+Le 165 (2003): Le Yellow Submarine
+Le 164 (2002): La drakkar
+Le 163 (2001): L'aéropostal
+Le 162 (2000): Le lance missile (dit la bite de Wizz)
+Le 161 (1999): Le quadripode de Star Wars (qui s'inclina bien involontairement devant St Waudru)
+Le 160 (1998): Le galion
+Le 159 (1997): Le CLIX (module martien)
+Le 158 (1996): La bétonneuse
+Le 157 (1995): Le camion de pompiers
+Le 156 (1994): L'hélicoptère Riptide (Seaking)
+Le 155 (1993): La navette spatiale
+Le 154 (1992): Le drakkar
+Le 153 (1991): Le tank
+
+Remarquez la finesse du camouflage...
+Le 152 (1990): Le transpolytech
+L'image reprise ici est en réalité une carte postale de l'époque... idée à refaire? (merci Patrick  )
+Le 151 (1989): Le chargeur sur pneus Caterpillar
+A l'époque, le char était fait dans la chaufferie sous la cité...
+Le 150 (1988): La moissoneuse bateuse...
+ Le 149 (1987): ...
+Le 148 (1986): ...
+Le 147 (1985): ...
+Le 146 (1984): Le train
+Le 145 (1983): ...
+Le 144 (1982): La calèche (la voiture hippomobile)
+Le 143 (1981): La navette spatiale
+Le 142 (1980): Le lit à baldaquins à porteurs (Des femmes en Polytech!!!)
+Le 141 (1979): Le landeau (Année de l'enfance)
+Le 140 (1978): L'amphore (à bleus porteurs)
+Le 139 (1977): Le géant "Boyard 1er" (Terme très utilisé à l'époque pour désigner, au départ, une andouille, et par la suite... un peu tout le monde)
+Celui-ci serait le premier char digne de ce nom (dont l'initiateur est Verjus).
+Le 138 (1976): La traction à bleus (On peut dire que c'est l'ancêtre de notre actuel char, avant que le véhicule ne soit surmonté d'un objet décoratif quelconque)
+Un véhicule supplémentaire s'était glissé dans le cortège de l'époque: La charette (celle-ci date des années 30 et provient d'une ancienne brasserie de Jemappes - C'est grâce à un bleu de l'époque qu'elle avait été prêtée)
+
+L'objet qui suit, accompagnant les 2 autres et à bleus porteurs, a été jugé "trop shoking" pour l'époque, ce qui lui a vallu de faire la moitié du cortège recouvert de toges! La censure en est la conséquence...
+
+Et enfin, 2 chars dont je ne connaîs pas l'année d'érection:
+
+le premier hélicoptère (Polytech Air Force)
+
+Le bateau à aubes
+
+-->
