e09108666ff01da459c29c51160f25768ceb5564
BlueTorche <dubrulle_kevin@hotmail.be>
2024-09-15 11:20:11 +0200

    adds some commentary of stuffs in bar.php

--- articles/cerclefedere/bar.php
+++ articles/cerclefedere/bar.php
@@ -5,3 +5,8 @@ baseArticle($title,$id);
 ?>
 
 <?php defaultArticle(); ?>
+
+<!--
+( Au bar, la bière a été à 25 francs pendant mes 7 ans à la fac (1994-2001) - juste avant, c'était 15 ou 20 francs (<1994), et juste après, c'est passé en euros (>2001)).
+Olivier Delbecke - 156
+-->
