ff402fbcd5bfb2d7d8dc95f1d7d3ba099e41e49e
BlueTorche <dubrulle_kevin@hotmail.be>
2024-09-15 11:18:02 +0200

    adds some commentary of stuffs in radio.php

--- articles/cerclefedere/radio.php
+++ articles/cerclefedere/radio.php
@@ -5,3 +5,20 @@ baseArticle($title,$id);
 ?>
 
 <?php defaultArticle(); ?>
+
+<!--
+J'étais tous les jours à la radio, mais pas dans le comité - j'ai perdu 5 x les élections radio, et gagné la 6e fois (trésorier radio),
+mais comme j'avais gagné (contre toute attente) à la frontalière quelques jours avant, j'ai dû refuser le poste.
+Ma présence à la radio et mes échecs électoraux m'ont valu le titre de "concierge radio" pendant plusieurs années.
+J'avais mon tiroir sous le gros frigo bleu, et je dormais presque tous les jours dans le fauteuil de la radio.
+
+En général, la radio était calme de 18h à 21h, puis il y avait plus de monde. Après le torchage du bar, il était très courant de faire une "after" à la radio.
+Il y avait des capotes sur les détecteurs de fumée, et de la bière (de mauvaise qualité Gluck/Karlsquell/Cara) à 20 francs,
+au début en libre service dans un frigo avec un panier pour y mettre les pièces, puis avec un distributeur dans lequel on avait juste le droit de mettre des produits Coca-Cola.
+( Au bar, la bière a été à 25 francs pendant mes 7 ans à la fac - juste avant, c'était 15 ou 20 francs, et juste après, c'est passé en euros ).
+Tous les jours émission matinale de 7h à 8h, et le soir de 18h à minuit. Le lundi, spéciale de 22h à minuit (souvent des bourgeois).
+J'ai fait le site web de la radio pendant quelques années. On avait la fréquence 106.9 FM, et le numéro de téléphone était le 065 37 46 38 ( je m'en souviens !).
+On avait une table de mix, deux lecteurs de cassettes, deux lecteurs de CD et trois micros. ( Pas de PC et pas d'internet évidemment )
+
+Olivier Delbecke (Baptisés 1994 - 156)
+-->
