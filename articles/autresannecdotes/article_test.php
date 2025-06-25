86ebf6e173c44b8d82bf730a47556a0fb5bbabd6
CommissionHistoriqueFPMS <historique.fpms@gmail.com>
2025-06-25 15:33:56 +0200

    Create article_test.php

---
+++ articles/autresannecdotes/article_test.php
@@ -0,0 +1,17 @@
+<?php
+$title ="Des héros de guerre : 1940 – 1945";
+$id = "herosguerre";
+baseArticle($title,$id);
+
+
+
+addImage("/image/cerclemons/isic.jpg", 500, "left");
+?>
+
+<p>
+  zseltkjmgzlekjgfj posdglkmnh zpmeokjgfi qzsdfpo
+</p>
+<?php 
+addImage("/image/cerclemons/isic.jpg", 500, "left"); ?>
+<h4>Sous titre </h4>
+
