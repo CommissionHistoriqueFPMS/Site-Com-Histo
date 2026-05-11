c0c9bd3f83e561612a89f379e928540b8dbd86f7
CommissionHistoriqueFPMS <historique.fpms@gmail.com>
2026-05-11 19:53:07 +0200

    Update listing-excel.php

--- articles/listingcomite/listing-excel.php
+++ articles/listingcomite/listing-excel.php
@@ -5,4 +5,4 @@ baseArticle($title,$id);
 ?>
 
 
-<?php addSource('Listing des Anciens Comité - Excel','/articles/listingcomite/listing.xlsx') ?>
+<?php addSource('Listing des Anciens Comité - Excel','/articles/listingcomite/listing-xlsx188.xlsx') ?>
