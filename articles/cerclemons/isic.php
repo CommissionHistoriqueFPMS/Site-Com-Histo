e79460c1f8a8bb5afecd2f9b600b7aee82382132
Hugo Quintart <98947898+HugoQuintart@users.noreply.github.com>
2026-07-08 12:46:25 +0200

    Fix typos and improve text clarity in isic.php

--- articles/cerclemons/isic.php
+++ articles/cerclemons/isic.php
@@ -28,13 +28,13 @@ Comme ce fût initié il y a quelques années par Sun ****, je me suis remis sur
 Pour rappel, Jean-Philippe « Sun » Soleil fût baptisé un soir d’octobre 1999 – Rhooo le dinosaure – et moi quelques années plus tard en 2002…
 
 </p><p>
-Ayant fait des recherches, interviewés quelques anciens, je me suis permis de reprendre le texte de Sun (parût dans le béati d’avril 2003) pour le compléter et vous faire part de quelques traces historiques… Traces parfois bien difficiles à retrouver… Certainement gardées dans le plus grand des secret au fin fond d’un Maître du temps bien particulier !
+Ayant fait des recherches, interviewé quelques anciens, je me suis permis de reprendre le texte de Sun (paru dans le béati d’avril 2003) pour le compléter et vous faire part de quelques traces historiques… Traces parfois bien difficiles à retrouver… Certainement gardées dans le plus grand des secrets au fin fond d’un Maître du temps bien particulier !
 
 </p><p>
 « Origine, je te tiens… » Disait Jean-Phi… Et bien, c’était vrai !
 
 </p><p>
-Les quelques nouveautés que je vais apporter corroborent bien avec ce qu’il avait annoncé autrefois dans le plus populaire des journaux estudiantins. Si par malheur, quelques esprits trop sages venaient à dire que ce ne sont ici qu’un ensemble de supputations et de faits non avérés, je le prierai de se rendre au local 500, de me payer une 33 et de m’accorder un entretien.
+Les quelques nouveautés que je vais apporter corroborent bien ce qu’il avait annoncé autrefois dans le plus populaire des journaux estudiantins. Si par malheur, quelques esprits trop sages venaient à dire que ce ne sont ici qu’un ensemble de supputations et de faits non avérés, je les prierais de se rendre au local 500, de me payer une 33 et de m’accorder un entretien.
 
 </p><p>
 Il est évident que ce texte est un rassemblement de différents faits et sources (texte de Sun, témoignages, récits d’anciens,…). Toute ressemblance ou reprise est donc probable. Que Dieu me pardonne !
@@ -55,13 +55,13 @@ Et bien, force de curiosité ou simplement coup du hasard, voilà qu’après pl
 Entassez-vous bien dans les banquettes fraîchement dépoussiérées du bar, prenez votre pilson et en avant pour le retour aux origines…
 
 </p><p>
-Arrêtons-nous dans les années 50 ! L’ISIC n’est même pas encore sortie des testicules de son papa… L’IRAM dispense une formation d’ingénieurs techniciens ; études en trois ans se donnant dans l’enceinte de l’école Saint Luc actuelle. Les cours sont principalement donnés par des Frères (lève ton verre…). Il n’est donc pas imaginable d’organiser quoique ce soit comme activité ayant un caractère bibitif, paillard, fokloriste ou autres bleusailles. Et pourtant, un certain folklore estudiantin se vit et se perpétue. Certains étudiants s’achètent une calotte (ooouuhhh !) qui ressemble fortement aux calottes que l’on peut voir dans les cercles actuels. Ce couvre – chef est donc porté à l’occasion de sorties en ville. A cette époque, il faut aussi préciser qu’un certain nombre d’élèves faisaient leurs études à Saint Luc en technique supérieure (équivalent des études A2 actuelles) pour ensuite poursuivre leur formation en faisant des études d’ingénieur Technicien (études A 1). La calotte (ou calot) faisait donc partie intégrante du costume des élèves de Saint Luc. Les étudiants poursuivant leurs études en ingéniorat, passant donc de A2 à A1, continuaient à porter leur calot pour les sorties en ville.
+Arrêtons-nous dans les années 50 ! L’ISIC n’est même pas encore sortie des testicules de son papa… L’IRAM dispense une formation d’ingénieurs techniciens ; études en trois ans se donnant dans l’enceinte de l’école Saint Luc actuelle. Les cours sont principalement donnés par des Frères (lève ton verre…). Il n’est donc pas imaginable d’organiser quoi que ce soit comme activité ayant un caractère bibitif, paillard, foklorique ou autres bleusailles. Et pourtant, un certain folklore estudiantin se vit et se perpétue. Certains étudiants s’achètent une calotte (ooouuhhh !) qui ressemble fortement aux calottes que l’on peut voir dans les cercles actuels. Ce couvre–chef est donc porté à l’occasion de sorties en ville. A cette époque, il faut aussi préciser qu’un certain nombre d’élèves faisaient leurs études à Saint Luc en technique supérieure (équivalent des études A2 actuelles) pour ensuite poursuivre leur formation en faisant des études d’ingénieur Technicien (études A 1). La calotte (ou calot) faisait donc partie intégrante du costume des élèves de Saint Luc. Les étudiants poursuivant leurs études en ingéniorat, passant donc de A2 à A1, continuaient à porter leur calot pour les sorties en ville.
 
 </p><p>
 Voilà donc la véritable origine du couvre-chef chez nous…
 
 </p><p>
-Cependant, des étudiants venaient d’autres écoles secondaires, ne portant donc pas de calots, se sont dirigés vers le port d’une « casquette à longue visière » ; très à la mode en ce temps-là. Se rappelant que la longue visière de la penne n’a d’autre but que d’empêcher de voir Dieu (où d’être vu par ce dernier…question de point de vue !), on comprend aisément pourquoi les étudiants venant de St Luc avaient optés pour la calotte !
+Cependant, des étudiants venaient d’autres écoles secondaires, ne portant donc pas de calots, se sont dirigés vers le port d’une « casquette à longue visière » ; très à la mode en ce temps-là. Se rappelant que la longue visière de la penne n’a d’autre but que d’empêcher de voir Dieu (où d’être vu par ce dernier…question de point de vue !), on comprend aisément pourquoi les étudiants venant de St Luc avaient opté pour la calotte !
 
 </p><p>
 Ceci dit, à cette époque, des baptêmes sont déjà organisés depuis plus d’un siècle à la Faculté Polytechnique (113ème promotion en 1951). Il n’est donc pas non plus improbable que certains étudiants Iramiens allaient se faire baptiser chez les ingénieurs civils qui étaient, eux, pennés.
@@ -78,9 +78,9 @@ Et puis le temps passe, la société évolue. Des activités d’accueil s’org
 </p><p>
 Citation du texte de Sun :
 </p><p>
-J’ai à ce propos reçu quelques photos des bleusailles organisées en 1962 (promotion de M. Franz Ansieau, membre de l’ADISIC. C’est lui qui m’a fournit les documents). Le principe du baptême est légèrement différent de ce qui se vit en nos jours mais l’esprit semble le même, toujours dans une ambiance bon enfant. On peut y voir les comitards, encore calottés en cette année et… les premières pennes ISIC ! De couleur lie-de-vin, elles sont tout à fait identiques à celles que nous portons aujourd’hui ! L’année d’apparition de la penne à l’ISIC se précise donc de plus en plus mais il persiste toujours un léger flou… C’est alors que j’ai eu l’occasion de discuter avec M. Basile Zampunieris, électromécanicien sorti également en 1962. Monsieur Zampunieris a su, comme tout bon ingénieur, trouver un équilibre entre le travail et l’amusement et a choisi de faire ses bleusailles. Le cycle d’étude étant toujours de trois ans, il a donc été baptisé en 1959. Et c’est là qu’il m’apprend que c’est justement cette année que les étudiants Iramiens se sont décidés à passer à la penne. En effet, la Polytechnique, Waroquier et la Fucam étaient tous pennés, alors pourquoi pas les Iramiens ?
+J’ai à ce propos reçu quelques photos des bleusailles organisées en 1962 (promotion de M. Franz Ansieau, membre de l’ADISIC. C’est lui qui m’a fourni les documents). Le principe du baptême est légèrement différent de ce qui se vit en nos jours mais l’esprit semble le même, toujours dans une ambiance bon enfant. On peut y voir les comitards, encore calottés en cette année et… les premières pennes ISIC ! De couleur lie-de-vin, elles sont tout à fait identiques à celles que nous portons aujourd’hui ! L’année d’apparition de la penne à l’ISIC se précise donc de plus en plus mais il persiste toujours un léger flou… C’est alors que j’ai eu l’occasion de discuter avec M. Basile Zampunieris, électromécanicien sorti également en 1962. Monsieur Zampunieris a su, comme tout bon ingénieur, trouver un équilibre entre le travail et l’amusement et a choisi de faire ses bleusailles. Le cycle d’étude étant toujours de trois ans, il a donc été baptisé en 1959. Et c’est là qu’il m’apprend que c’est justement cette année que les étudiants Iramiens se sont décidés à passer à la penne. En effet, la Polytechnique, Waroquier et la Fucam étaient tous pennés, alors pourquoi pas les Iramiens ?
 </p><p>
-Il serait donc bon de croire que la penne fût définitivement choisie à la fin des années 50(57 – 58 – 59). A cette période, l’exposition universelle allait ouvrir ces portes à plusieurs dizaines de milliers de visiteurs et certains étudiants de la FPMs allaient dérober le dragon quelques jours avant la ducasse de la Cité Porte-Bonheur.
+Il serait donc bon de croire que la penne fut définitivement choisie à la fin des années 50(57 – 58 – 59). A cette période, l’exposition universelle allait ouvrir ses portes à plusieurs dizaines de milliers de visiteurs et certains étudiants de la FPMs allaient dérober le dragon quelques jours avant la ducasse de la Cité Porte-Bonheur.
 
 </p><p>
 Les faits suivants pouvant confirmer cette fourchette d’années : cette date correspond à la fin du port du costume dans le secondaire et donc de la calotte ; mais correspond également au 4 ou 5ème anniversaire de notre cercle estudiantin (date de naissance avoisinant l’année 1955). Il est aussi à remarquer, que la couleur de la penne rappelle celle du dessus du calot de Saint Luc…
@@ -89,7 +89,7 @@ Les faits suivants pouvant confirmer cette fourchette d’années : cette date c
 Ainsi, logiquement tous les étudiants baptisés de mon année (2002) sont donc de la 44ème promotion (soit quarante trois années de pennés) et nous ouvrirons les portes du folklore à la 49ème promotion en septembre.
 
 </p><p>
-Comme fût le souhait de Sun et le mien, une guindaille grandiose pourrait être mise en place pour le demi-siècle de notre cercle isicien ? Espérons tous ensemble que cet anniversaire sera bien fêté ; que tout aille bien pour le comité et le cercle.
+Comme fut le souhait de Sun et le mien, une guindaille grandiose pourrait être mise en place pour le demi-siècle de notre cercle isicien ? Espérons tous ensemble que cet anniversaire sera bien fêté ; que tout aille bien pour le comité et le cercle.
 
 </p><p>
 Des bleus, des bleus, des bleus !
