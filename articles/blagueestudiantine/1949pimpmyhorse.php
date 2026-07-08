ab984524050bb96d27d14dd08144a2348b563a5e
Hugo Quintart <98947898+HugoQuintart@users.noreply.github.com>
2026-07-08 12:36:10 +0200

    Fix typo in 1949pimpmyhorse.php

--- articles/blagueestudiantine/1949pimpmyhorse.php
+++ articles/blagueestudiantine/1949pimpmyhorse.php
@@ -8,7 +8,7 @@ baseArticle($title,$id);
 <?php addImage("/image/blagueestudiantine/pimpmyhorse1949.webp", 200, "left") ?>
 
 <p>
-    La traditionnelle blague estudiantine dont nous ne sommes pas prêts de retrouver les origines avait déjà
+    La traditionnelle blague estudiantine dont nous ne sommes pas près de retrouver les origines avait déjà
     une belle place en 1949 ( date à vérifier mais doit être plus ancienne ).
 </p>
 <p>
