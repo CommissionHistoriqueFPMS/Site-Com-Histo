85654597288accaaa2ddee6eb72231910112b796
Hugo Quintart <98947898+HugoQuintart@users.noreply.github.com>
2026-07-10 09:51:56 +0200

    visites roi

--- articles/autresanecdotes/visiteroi.php
+++ articles/autresanecdotes/visiteroi.php
@@ -1,10 +1,19 @@
 <?php
-$title ="Visites Royales";
+$title = "Visites Royales";
 $id = "visiteroi";
-baseArticle($title,$id);
-
+baseArticle($title, $id);
+ 
 addSource("site comhisto", "https://historiquefpms.wordpress.com/la-fac/");
 ?>
-
-<?php defaultArticle(); ?>
-
+ 
+<p>Notre Faculté a eu l'honneur de recevoir la visite de plusieurs souverains belges au fil de son histoire.</p>
+ 
+<h4>Léopold III</h4>
+<?php addImage("/image/autresanecdotes/leo3.jpg", 500); ?>
+ 
+<h4>Baudouin I<sup>er</sup></h4>
+<p>Le 4 octobre 1962 (au théâtre royal, nous semble-t-il).</p>
+<?php addImage("/image/autresanecdotes/baudoin.jpg", 500); ?>
+ 
+<h4>Albert II</h4>
+<p>Lors de la remise des diplômes et insignes de Docteur <em>Honoris Causa</em> en 1997, qui furent remis par le doyen de l'époque, M. Pilatte, au Docteur Didier Dubois, au professeur Horst Kruse et enfin au professeur Zenon Mroz.</p>
