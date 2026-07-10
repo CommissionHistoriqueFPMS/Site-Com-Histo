468c80169df04234904d1d4041938084a20cc430
Hugo Quintart <hugoquintart@gmail.com>
2026-07-10 16:10:11 +0200

    Ajout de l'article sur le scandale du festival 1988 + Modification margin cadre body + ajout style pour chant + modif image accueil

---
+++ articles/vieetudiante/1988festivalchantscandale.php
@@ -0,0 +1,95 @@
+<?php
+$title = "Scandale au festival de la chanson 1988";
+$id = "scandalefestivalchanson88";
+baseArticle($title, $id);
+?>
+<?php addImage("/image/vieetudiante/fest.jpg", 400, "right", "border: 3px solid var(--derive-main-color); border-radius: 8px; padding: 4px; background: #fff;") ?>
+<p>
+    Nous sommes dans les années 80, depuis quelques années la rivalité entre la F.P.Ms et la Polytech ULB est grandissante.
+    
+    Lors des précédents festivals, les Polytech de Mons taquinaient les ULB’s en terminant leurs chansons par des : « ULB ULB Mons t’encule » ou encore par des « Bruxelles c’est du caca » et de leur coté les Polytech ULB imprimaient des autocollants sur lesquels il était marqué : « Mons c’est du caca » A cette époque, il faut rappeler que les fossiles commençaient à être acceptés parmi les étudiants baptisés de l’ULB ce qui était très mal vu par d’autres facs plus folkloriques (la nôtre par exemple).
+</p>
+<p >En 1988, à une époque où les esprits de certains étudiants n’étaient pas encore trop sages, la Polytech de Mons s’est rendue chez les bruxélèèèèèères pour participer au concours de chants comme elle le faisait chaque année.</p>
+<br>
+<p >En collaboration avec les Fuc(kh)ams, ils chantèrent une composition insultant l’ULB, les traitant de fossiles ou encore d’enculés. La suite est logique, les ULBistes ont réagit et une méchante bagarre, à coup de poings et de chaines, a éclaté. Cette année le festival s’est terminé plus tôt qu’à l’habitude pour cause « d’affaire montoise » comme ils disent. Voici le chant chanté par les Montois à l’ULB, ce chant a été écrit en grande partie par des Fucam’s et chanté par les Montois. A vous de juger !</p>
+<div class="chant">
+    <p class="chant-air">Air : les Champs-Élysées</p>
+    <p >Mons est là pour tout casser<br>
+    Pour s’amuser, tout faire foirer<br>
+    Pour s’en foutre derrière la cravate<br>
+    Et sans rien payer.<br>
+    On vous aura quoiqu’vous fassiez<br>
+    Car tout compte fait, en vérité<br>
+    Vous n’êtes quand même qu’des empotés<br>
+    A l’ULB</p>
+    <p >Refrain</p>
+    <p >A l’ULB la, la, la,… (bis)<br>
+    C’est bourré de pédés, de poufiasses, d’enculés<br>
+    Il y a tout ce que vous voulez<br>
+    A l’ULB</p>
+    <p >Moi, l’sida, j’en ai pas peur<br>
+    Je l’ai déjà d’puis tout à l’heure<br>
+    J’avais envie de foutre ma bite<br>
+    Dans n’importe qui. N’importe qui et ce fut toi<br>
+    J’aurais du dire n’importe quoi,<br>
+    Elles ont vraiment le trou trop gras<br>
+    A l’ULB</p>
+    <p >Cachez vos pennes, bande de fossiles<br>
+    Y’a pas de baptême dans votre asile<br>
+    Mais vous savez vous étoiler<br>
+    Comme de gros busés.<br>
+    Faut dire que vous en foutez par pelle<br>
+    Des argentées et des dorées<br>
+    Pour marquer l’imbécilité<br>
+    De l’ULB</p>
+    <p >L’ULB sait pas guindailler<br>
+    Dans leurs fauteuils comme des pépés,<br>
+    Ils sont là à nous écouter<br>
+    Comme des enfoirés.<br>
+    Faudrait veiller à vous r’muer<br>
+    A boire des pintes, vous amuser<br>
+    Comme les Montois sur le plancher<br>
+    De l’ULB</p>
+    <p >Après cette histoire, la Polytech de Mons n’a pas pu remettre les pieds au festival du chant estudiantin et le port des chaines a été interdit lors des suivants festivals.</p>
+    <p >L’année suivante (1989), en réponse aux Polytech de Mons, les bruxellois ont créé cette nouvelle chanson :</p>
+    <p >« Le bétail montois »<br>
+    Air : Le jouet extraordinaire (C. François)</p>
+    <p >Ils sont entrés comme des boeufs,<br>
+    Dans cet auditoire du Janson<br>
+    Où tout était prévu pour eux,<br>
+    Oui, prévu pour eux.<br>
+    Dés qu’ils firent leur apparition,<br>
+    Ils ne pouvaient en croire leurs yeux,<br>
+    La bière coulait à foison<br>
+    Aux frais de la maison.</p>
+    <p >Descendre sur la capitale,<br>
+    Pour assister au Festival,<br>
+    S’ils sont Montois, faut pas rêver<br>
+    Non, faut pas rêver.<br>
+    Ce n’ serait vraiment pas normal,<br>
+    S’ils étaient venus pour chanter,<br>
+    Sortis de leur jungle natale<br>
+    Faut pas trop demander.</p>
+    <p >Le résultat de leur passage<br>
+    Fit le bonheur des vitriers,<br>
+    Même Attila n’eut pu commettre<br>
+    Un si grand carnage.<br>
+    Mais tous ensemble nous le jurons,<br>
+    Nous n’ leur en voulons pas du tout,<br>
+    C’est incurable nous le savons<br>
+    Le fait d’être con.</p>
+    <p >Je tiens enfin à saluer<br>
+    Ceux qui se sont sentis floués,<br>
+    Qu’ils se rassurent leurs deniers<br>
+    N’ sont pas égarés.<br>
+    Mais il a bien fallu trouver<br>
+    Un tout petit peu de monnaie,<br>
+    Pour assainir la comptabilité du CP !</p>
+</div>
+<p >Évidemment, étant banni du festival, nous n’avons pas pu réagir ! Au fil du temps, cette rivalité s’est estompée, d’ailleurs il parait que le folklore bruxellois a subi le même sort. Comme quoi nous ne sommes pas tout à fait innocents! C’est d’ailleurs suite à ces malheureux événements que les chaines ont été proscrites durant le festival.</p>
+<h3 >..retour au festival 2007</h3>
+<?php addImage("/image/vieetudiante/1999.jpg", 400, "left", "border: 3px solid var(--derive-main-color); border-radius: 8px; padding: 1px; background: #fff;") ?>
+<p >Lors du 33e festival, une délégation issue de notre belle faculté fit son grand retour après presque 20 ans d’absence et ce malgré des invitations datant d’au moins 1999. C’est en 2006 qu’un certain engouement pour cette manifestation folklorique renaît. Cependant c’est en 2007 que nous avons fait notre grand retour, malgré une tentative avortée l’année précédente. Et notre retour fut plus que festif et chaleureux, accueilli comme des rois par nos homologues bruxellois, nous avons chanté, nous avons affoné, bref nous avons guindaillé tous ensembles.</p>
+<p >En parlant de chant, celui présenté par nos six chanteurs de l’époque (Seb Vaesen, Thomas Raes, Gilles Noël, Raph Nardo, Sebastiano Tudisco et Nicolas May) a remporté l’un des plus beaux prix du festival : Le prix de la chanson représentant le mieux les valeurs de l’ULB.</p>
+<p >Ce prix résulte bien de la mentalité mise en œuvre notamment par le comité fédé de l’époque dont le but était de renouer une saine amitié entre nous et nos homologues bruxellois et on peut dire que le pari fut plus que réussi. En effet, depuis lors, nous participons tous les ans au festival avec plus ou moins de succès et il n’est pas rare de recroiser quelques gens de la capitale durant nos guindailles.</p>
+<?php addImage("/image/vieetudiante/retour.jpg", 500, "center") ?>
