d1b08752128bb94d4156584633c97307a8187067
Hugo Quintart <hugoquintart@gmail.com>
2026-07-10 17:08:31 +0200

    Embed de l'interview de Claude Pépin sur le vol du doudou en 57

--- articles/blagueestudiantine/1957voldoudou.php
+++ articles/blagueestudiantine/1957voldoudou.php
@@ -15,5 +15,5 @@ baseArticle($title,$id);
 <p>
     L’identité des voleurs est restée inconnue de la plupart des Montois mais ils ne sont pas restés cachés longtemps.
 </p>
-
+<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay; encrypted-media" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A66469135&color=%237e1517&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/commission-historique" title="Commission Historique" target="_blank" style="color: #cccccc; text-decoration: none;">Commission Historique</a> · <a href="https://soundcloud.com/commission-historique/claude-p-pin-vol-du-doudou-en" title="Claude Pépin - Vol du Doudou en 1957" target="_blank" style="color: #cccccc; text-decoration: none;">Claude Pépin - Vol du Doudou en 1957</a></div>
     <?php addSource('Interview Ernest Carlier (TeleMB)','https://www.youtube.com/watch?v=ZSOuI5yNj6U') ?>
