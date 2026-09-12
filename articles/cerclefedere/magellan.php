195eef00452438acbf16aa01a39254a68c207e4f
Hugo Quintart <hugoquintart@gmail.com>
2026-09-12 15:16:47 +0200

    Au Magellan, on est lent, et on affonne comme des glands

--- articles/cerclefedere/magellan.php
+++ articles/cerclefedere/magellan.php
@@ -1,29 +1,71 @@
 <?php
-$title ="Cercle Magellan";
-$id = "magellan";
-baseArticle($title,$id);
+$title = "Cercle Magellan";
+$id    = "magellan";
+baseArticle($title, $id);
 ?>
 
-<?php defaultArticle(); ?>
+<p>
+    Le Cercle Magellan est le cercle informatique de la Faculté Polytechnique de Mons. Il est composé d'un groupe
+    d'étudiants appelés Admins. Cette fine équipe, passionnée par l'informatique, se regroupe régulièrement pour
+    jouer aux jeux vidéo ou pour réaliser des projets et solutions informatiques tels que des sites web ou de
+    l'hébergement.
+</p>
+<p>
+    Le Magellan est également chargé des réseaux Internet des cités Houzeau et Franeau, et sert de HelpDesk pour
+    ces cités étudiantes.
+</p>
+<p>
+    Le Cercle Magellan organise aussi le GAME, une LAN party de 48 h où plus de 200 joueurs participent chaque
+    année à plusieurs tournois de jeux vidéo.
+</p>
+<p>
+    Si tu es intéressé par l'informatique, les jeux vidéo, ou simplement curieux d'apprendre, n'hésite pas à nous
+    rejoindre au local 500 ou à contacter un membre du comité !
+</p>
 
-<!--
+<h4>Local 500</h4>
+
 <p>
-    Le Cercle Magellan existe officiellement depuis 2002. Avant cette date, le Magellan était une partie du Cercle
-    Culturel, où nous pouvions retrouver un délégué Magellan, qui se chargeait alors de la gestion du local 500 et
-    du réseau. Cependant, nous pouvons retrouver des traces du Magellan encore plus tôt, sous la dénomination de "Club HP".
-    Le Club HP gérait alors le local 500, où nous pouvions retrouver le seul ordinateur ayant accès au réseau
-    internet dans toute la cité.
+    Nous disposons d'un local au sein de la cité Houzeau (local 500), où nous mettons à disposition des PC en accès
+    libre avec une session privée. Les PC disponibles sont très performants et permettent aussi bien de jouer à tes
+    jeux préférés que de travailler sur des projets, le tout dans l'ambiance typique du Magellan.
+</p>
+
+<h4>Histoire</h4>
+
+<p>
+    À l'arrivée d'Internet dans la cité Houzeau, plusieurs personnes se sont découvert une passion pour cela et se
+    sont donné comme objectif d'apporter ce flux de données dans tous les kots. Pour y parvenir, ils ont tendu des
+    câbles entre les kots, étage par étage, par l'extérieur et les balcons. Il y avait donc un responsable du fil
+    d'arrivée pour tout l'étage.
 </p>
 <p>
-    Durant les campagnes des années 2000 se déroule la première édition du GAME, organisés par le comité candidat
-    Q2000. Cette première édition se déroulait alors à la salle PP. C'est uniquement lors de la quatrième édition
-    du GAME que celui-ci migre dans l'Amphithéâtre Stievenart, où il se déroule encore aujourd'hui.
+    Il existe d'ailleurs beaucoup d'anecdotes : par exemple, certains bourgeois allaient se plaindre car leur
+    responsable débranchait tous les autres de l'étage pour télécharger plus vite ! Le réseau de la cité était né,
+    et déjà autogéré par les étudiants.
 </p>
 <p>
-    Le réseau internet de la cité Houzeau a toujours été introduit par le Club HP, avant d'être repris par le
-    Magellan lors du changement de nom. Avant l'installation du wifi et l'installation propre des cables réseau
-    par la faculté, le réseau était géré dans le local 500. Les câbles réseaux quittaient le local pour rejoindre
-    l'un des kots en face, et les câbles sortaient ensuite par la fenêtre du kot, pour rejoindre d'autres kot afin
-    de donner du réseau internet aux résidents. La facade de la cité ressemblait alors à une grande toile
-    d'arreignée.
-</p>-->
+    À l'origine, le local 500 était sous la responsabilité du Cercle Culturel, avec un délégué Magellan. Le Culturel
+    ayant une trop grande charge de travail et le réseau demandant de plus en plus d'implication, il a été décidé en
+    2002 de rendre le Magellan indépendant.
+</p>
+
+<h4>Oripeau du comité</h4>
+
+<p class="bold-text">Original</p>
+<p>
+    Lors d'un cortège des bleus, le président de l'époque aperçoit un bleu avec une capuche de nounours blanc. Dofus
+    étant à la mode, il pense directement à la coiffe du Bouftou. Le président est donc allé lui demander gentiment
+    de la récupérer. Pour renforcer la ressemblance, un tapis a été ajouté : la coiffe était née.
+</p>
+<p>
+    Bien plus tard, lors d'un autre cortège des bleus, le secrétaire de l'époque, Lucas Lebrun, aperçoit une bleuse
+    en mouette, avec deux ailes en tapis IKEA ressemblant à la coiffe du président. Il lui demanda gentiment de les
+    lui donner : les coiffes du trésorier et du secrétaire étaient nées.
+</p>
+
+<p class="bold-text">Nouveau</p>
+<p>
+    Après plusieurs remarques des autres cercles dues à la tendance de la coiffe à se salir, et sur l'envie du
+    président, la coiffe fut remplacée par un drapeau.
+</p>
